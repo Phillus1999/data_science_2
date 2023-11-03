098464de5565c926689b2168a2624efc19c3b39a
Daniel Warkus <daniel.warkus@sesoco.de>
2023-11-03 14:13:40 +0100

    Reducer erstellt

--- projects/uebung_3/aufgabe2.py
+++ projects/uebung_3/aufgabe2.py
@@ -9,6 +9,14 @@ class ShinglesCount(MRJob):
         :param line: Eine Zeile.
         :return: Generator der Shingles
         """
-        k = 3  
+        k = 3
         for i in range(len(line) - k + 1):
             yield line[i:i + k], 1
+
+    def reducer(self, shingle, counts):
+        """
+        :param shingle (Key):
+        :param counts (Value):
+        :return:
+        """
+        yield shingle, sum(counts)
